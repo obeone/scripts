696698e88457c2bf5440ea818ac9c535d2012719
Grégoire Compagnon <obeone@obeone.org>
2025-06-04 23:25:01 +0200

    docs(slideshow): add module docstring describing package initialization and main application logic

--- slideshow/src/slideshow/__init__.py
+++ slideshow/src/slideshow/__init__.py
@@ -1,1 +1,8 @@
 # slideshow package
+#
+# This module initializes the slideshow package.
+#
+# It provides the main application logic and core functionality for the
+# ImageSlideshowApp, which is a feature-rich Tkinter slideshow.
+#
+# To run the application, see slideshow.__main__.
